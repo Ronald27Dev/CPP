e70642d054125f24c2eca1a3f8d829412a9347e2
Ronald <ronald.mrodrigues@hotmail.com>
2024-10-23 17:03:12 -0300

    Adição das funcionalidades e views do Chat

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -8,6 +8,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        &nbsp;&nbsp;<a class="navbar-brand" href="#">CPP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Inicio <span class="sr-only">(Atual)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/chatify">Chat</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+    <br><br>
+    
     <div class="container">
         <div class="card">
             <div class="card-header">
